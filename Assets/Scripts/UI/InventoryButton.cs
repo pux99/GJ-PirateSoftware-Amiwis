1849872f0f79ebca2aec208f1af96e4a9ec2fc6b
WishedClover48 <matuleon2001@gmail.com>
2024-07-29 19:26:51 -0300

    Main Level

--- Assets/Scripts/UI/InventoryButton.cs
+++ Assets/Scripts/UI/InventoryButton.cs
@@ -26,6 +26,7 @@ public class InventoryButton : MonoBehaviour
     {
         buttonStartingPosition = transform.position;
         InventoryStartingPosition= inventory.transform.position;
+        //fullDistance = inventory.
     }
 
     // Update is called once per frame
